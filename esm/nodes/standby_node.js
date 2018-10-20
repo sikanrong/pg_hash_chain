db404ea97ace548ede6176cb59397587ed072e07
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2018-10-21 00:54:15 +0100

    Tweaking replenishSlaves a bit; correcting slave_lock_path

--- esm/nodes/standby_node.js
+++ esm/nodes/standby_node.js
@@ -88,7 +88,7 @@ class StandbyNode extends Node{
     replenishSlaves(){
         const watchSlaveLocks = async () => {
             for(let i = 0; i < $config.pg_slave_count; i++){
-                const slave_lock_path = path.join(path.dirname(this.slave_lock_path), '..', i);
+                const slave_lock_path = path.join(`/lock/slave/${this.zk_myid}`, i);
                 const gc_reply = await this.zk.getChildren(slave_lock_path, true);
                 if(gc_reply.children == 0){
                     //spin up a new process
