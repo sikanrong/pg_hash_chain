0a95c69e51be7c81b0b81dad5d700d8242d6a36e
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2018-10-21 23:54:03 +0100

    adjusting spawn arguments in postgres spawn

--- esm/nodes/standby_node.js
+++ esm/nodes/standby_node.js
@@ -116,7 +116,10 @@ class StandbyNode extends Node{
         const global_idx = ((this.is_master)? 0 : (this.slave_lock_held + 1));
         const pg_data_dir = path.join($config.pg_cluster_path, `node${global_idx}`);
 
-        const cp = spawn(`/usr/lib/postgresql/9.4/bin/postgres -D ${pg_data_dir} -p ${$config.pg_port_start + global_idx}`);
+        const cp = spawn(`/usr/lib/postgresql/9.4/bin/postgres`, [
+            `-p ${$config.pg_port_start + global_idx}`,
+            `-D ${pg_data_dir}`
+        ]);
         const ws = fs.createWriteStream(`${$config.app_deploy_path}/current/tmp/psql${global_idx}.log`);
         cp.stdout.pipe(ws);
         cp.stderr.pipe(ws);
